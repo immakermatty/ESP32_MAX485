903cb9186a31f780687c15dd150ac057e04bab57
Matěj Suchánek <kkartk322@gmail.com>
2021-03-24 16:10:30 +0100

    fixing DMX protocol problems

--- src/MAX485_DMX.cpp
+++ src/MAX485_DMX.cpp
@@ -16,40 +16,32 @@
 #include "MAX485_DMX.h"
 
 #define SERIAL_BAUDRATE_DMX 250000
-#define SERIAL_CONFIG_DMX SERIAL_8N2
-#define SERIAL_BAUDRATE_BREAK 76800
-#define SERIAL_CONFIG_BREAK SERIAL_8N1
+#define SERIAL_BAUDRATE_BREAK 57600
+#define SERIAL_CONFIG SERIAL_8N2
 
 #define DMX_MAX_CHANNEL 512
 #define DMX_MIN_CHANNEL 1
 #define DMX_MIN_TXSLOTS 24
 
-MAX485_DMX::MAX485_DMX(HardwareSerial& serialInterface)
+MAX485_DMX::MAX485_DMX(int8_t di_pin, int8_t de_pin, int8_t re_pin, int8_t ro_pin, HardwareSerial& serialInterface)
     : m_serial(serialInterface)
-    , m_txPin(-1)
-    , m_rxPin(-1)
+    , m_txPin(di_pin)
+    , m_rxPin(ro_pin)
     , m_dmxSlots {}
 {
-    memset(m_dmxSlots, 0, sizeof(m_dmxSlots));
-}
-
-void MAX485_DMX::begin(int8_t diPin, int8_t dePin, int8_t rePin, int8_t roPin)
-{
-    m_txPin = diPin;
-    m_rxPin = roPin;
-
-    // dePin & rePin together HIGH => MAX485 is a transmitter
+    // de_pin & re_pin together HIGH => MAX485 is a transmitter
     // LOW => MAX485 is a receiver
 
     //lets set it pernamently as a transmitter
-    if (dePin != -1) {
-        pinMode(dePin, INPUT_PULLUP);
-    }
-    if (rePin != -1) {
-        pinMode(rePin, INPUT_PULLUP);
-    }
+    pinMode(de_pin, INPUT_PULLUP);
+    pinMode(re_pin, INPUT_PULLUP);
 
-    m_serial.begin(SERIAL_BAUDRATE_DMX, SERIAL_CONFIG_DMX, m_rxPin, m_txPin);
+    memset(m_dmxSlots, 0, sizeof(m_dmxSlots));
+}
+
+void MAX485_DMX::begin()
+{
+    m_serial.begin(SERIAL_BAUDRATE_DMX, SERIAL_CONFIG, m_rxPin, m_txPin);
 }
 
 void MAX485_DMX::end()
@@ -88,16 +80,18 @@ void MAX485_DMX::writeOut(size_t channels, bool wait)
     if (channels > DMX_MAX_CHANNEL)
         channels = DMX_MAX_CHANNEL;
 
+    m_dmxSlots[0] = 0x00;
+
     // sending break
-    m_serial.begin(SERIAL_BAUDRATE_BREAK, SERIAL_CONFIG_BREAK, m_rxPin, m_txPin);
-    m_serial.write(0);
+    m_serial.updateBaudRate(SERIAL_BAUDRATE_BREAK);
+    m_serial.write(0x00);
     m_serial.flush();
 
     // sending data
-    m_serial.begin(SERIAL_BAUDRATE_DMX, SERIAL_CONFIG_DMX, m_rxPin, m_txPin);
+    m_serial.updateBaudRate(SERIAL_BAUDRATE_DMX);
     m_serial.write(m_dmxSlots, channels + 1);
-
-    if (wait) {
+    
+    if(wait) {
         m_serial.flush();
     }
 }
